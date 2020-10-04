4ea3b81112d1afed1182dfd6822fb26b3f37ebc2
Shivansh Srivastava <shivanshsrivastava2000@gmail.com>
2020-10-04 15:38:46 +0530

    Basic Page Added

--- newsapp/views.py
+++ newsapp/views.py
@@ -1,3 +1,21 @@
+# API Importing
 from django.shortcuts import render
+from newsapi import NewsApiClient
 
-# Create your views here.
+def index(request):
+    newsapi = NewsApiClient(api_key ='YOURAPIKEY')
+    top = newsapi.get_top_headlines(sources ='techcrunch')
+
+    l = top['articles']
+    desc =[]
+    news =[]
+    img =[]
+
+    for i in range(len(l)):
+        f = l[i]
+        news.append(f['title'])
+        desc.append(f['description'])
+        img.append(f['urlToImage'])
+    mylist = zip(news, desc, img)
+
+    return render(request, 'index.html', context ={"mylist":mylist})
