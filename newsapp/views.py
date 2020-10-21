ee2e4576e2896864c1f27ef62c9482fd9dc044f1
Shivansh Srivastava <shivanshsrivastava2000@gmail.com>
2020-10-21 12:45:15 +0530

    Country Dropdown | Views Fixed

--- newsapp/views.py
+++ newsapp/views.py
@@ -3,20 +3,28 @@ from django.shortcuts import render
 from newsapi import NewsApiClient
 
 import config
+
 def index(request):
+    countryvar = 'IN'
+    if request.method == "POST":
+        countryvar = request.POST['country']
     newsapi = NewsApiClient(api_key = config.api_key)
-    top = newsapi.get_top_headlines(sources ='techcrunch')
+    countryvar = countryvar.lower()
+    print(countryvar)
+    top = newsapi.get_top_headlines(country=countryvar)
 
     l = top['articles']
-    desc =[]
-    news =[]
-    img =[]
+    desc = []
+    news = []
+    img  = []
+    url  = []
 
     for i in range(len(l)):
         f = l[i]
         news.append(f['title'])
         desc.append(f['description'])
         img.append(f['urlToImage'])
-    mylist = zip(news, desc, img)
+        url.append(f['url'])
+    mylist = zip(news, desc, img, url)
 
     return render(request, 'index.html', context ={"mylist":mylist})
